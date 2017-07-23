3dfd1897cfc74e63a4049f3d915295331139cfb2
auzurdragon <auzurdragon@163.com>
2017-07-23 23:09:53 +0800

    pandas

--- pythonForDataAnalysis.py
+++ pythonForDataAnalysis.py
@@ -2,15 +2,17 @@
 path = 'http://bit.ly/ch02/usagov_bitly_data2012-03-16-1331923249.txt'
 
 import pymongo
-from urllib.parse import quote_plus
-
 conn = pymongo.MongoClient(host='112.74.161.9', port=28010)
 conn.BJDdb.authenticate('writeuser','51write')
+try:
+    db = conn.BJDdb
+    coll = db.BJD_User
+except expression as identifier:
+    print(identifier)
+
 db = conn.BJDdb
 coll = db.BJD_User
-
-rec = [i for i in coll.find(limit=100)] # 从数据库中取出100条用户记录
-
+rec = [i for i in coll.find(limit=100, sort=[('_id':-1)])] # 从数据库中取出100条用户记录
 conn.close()
 
 # 按nativename分组计数
@@ -39,3 +41,16 @@ def get_counts(sequence):
     print (dict(t))
     print (t.most_common(10))  # 打印top10
     return t
+
+def get_counts(sequence,countfield):
+    """使用pandas.DataFrame的数据框处理方法进行计数"""
+    from pandas import DataFrame
+    frame = DataFrame(sequence) # 将数据对象转换为DataFrame数据框
+    return frame[countfield].value_counts() # 使用value_counts()方法计数
+
+
+
+
+from pandas import DataFrame, Series
+import pandas as pd; import numpy as np
+frame = DataFrame(rec)
